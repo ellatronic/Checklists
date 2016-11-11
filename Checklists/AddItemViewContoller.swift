a76ee2bc61c863359c09b8e8d2e6b35b92193ec5
ellatronic <ellachiangg@gmail.com>
2016-11-11 10:58:02 -0800

    Disabled done button without text in field

--- Checklists/AddItemViewContoller.swift
+++ Checklists/AddItemViewContoller.swift
@@ -11,6 +11,7 @@ import UIKit
 
 class AddItemViewController: UITableViewController, UITextFieldDelegate {
     @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
     
     @IBAction func cancel() {
         dismiss(animated: true, completion: nil)
@@ -27,5 +28,14 @@ class AddItemViewController: UITableViewController, UITextFieldDelegate {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         textField.becomeFirstResponder()
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        let oldText = textField.text! as NSString
+        let newText = oldText.replacingCharacters(in: range, with: string) as NSString
+        
+        doneBarButton.isEnabled = (newText.length > 0)
+        return true
     }
 }
